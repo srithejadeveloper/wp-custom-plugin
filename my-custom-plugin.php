4a877fde47a7c34096b9553fe02655a645d83042
srithejadeveloper <115281690+srithejadeveloper@users.noreply.github.com>
2023-07-27 12:21:59 +0530

    Add files via upload

---
+++ my-custom-plugin.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Plugin Name: My custom plugin
+     * Plugin URI: https://wordpress.com
+     * Description: Plugin Description here.
+     * Author: Sritheja
+     * Version: 1.0.0
+     * Author URI: http://bhargavb.wordpress.com/
+     */
+
+     echo 'This is my test plugin';
+
+     ?>
